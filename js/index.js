985358ef7af1aee2445a82456f02b1728994671d
Carlos Gomez <cggomez93@gmail.com>
2023-03-13 16:49:17 +0100

    He hecho modificaciones

--- js/index.js
+++ js/index.js
@@ -3,36 +3,16 @@
 (()=>{
 
     const header       = document.querySelector('.header')
-    const headerMenu   = document.querySelector('.header__menu')
+    const headerMenu   = header.querySelector('.header__menu')
     const menu         = document.querySelector('.menu')
-    const menuA        = document.querySelectorAll('.menu__a') 
-    const worksImg     = document.querySelectorAll('.works__img')
-    const worksP       = document.querySelectorAll('.works__p')
-
-    
+    const menuA        = menu.querySelectorAll('.menu__a') 
     
     const headerMenuHandler = () => {
         menu.classList.toggle('active')
     }
-    
+
     headerMenu.addEventListener('click', headerMenuHandler)
-    
-    
-    worksImg.forEach((eachWork, i)=>{
-        worksImg[i].addEventListener('mouseover',()=>{
-            worksImg.forEach((eachWork, j)=>{
-                worksImg[i].classList.add('jello-vertical')
-            })
-        })
-    })
-    
-    worksImg.forEach((eachWork, i)=>{
-        worksImg[i].addEventListener('mouseout',()=>{
-            worksImg.forEach((eachWork, j)=>{
-                worksImg[i].classList.remove('jello-vertical') 
-             })
-        })
-    })
+
     
     menuA.forEach((eachA, i)=>{
         menuA[i].addEventListener('click',()=>{
@@ -46,6 +26,7 @@
         menuA[i].addEventListener('mouseover',()=>{
             menuA.forEach((eachA, j)=>{
                 menuA[i].classList.add('color') 
+                menuA[i].classList.add('desplazamiento') 
              })
         })
     })  
@@ -54,7 +35,9 @@
         menuA[i].addEventListener('mouseout',()=>{
             menuA.forEach((eachA, j)=>{
                 menuA[i].classList.remove('color') 
+                menuA[i].classList.remove('desplazamiento') 
              })
         })
     })  
+    
 })();
